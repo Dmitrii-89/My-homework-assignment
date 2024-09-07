75a3a7bf554d5808d8a5cfbf8514c61412790e1f
Dmitrii-89 <kabasya89@mail.ru>
2024-09-08 00:27:49 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+number_of_completed_tasks = '12'
+a = 12
+
+number_of_hours_spent = '1.5'
+b = 1.5
+
+course_name = 'Python'
+
+time_for_one_task = b / a
+
+print('Курс:',course_name,',всего задач:',number_of_completed_tasks,',затрачено часов:',number_of_hours_spent,',среднее время выполнения',time_for_one_task,'часа.')
