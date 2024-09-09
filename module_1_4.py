7a4ecf5017271360d70cc6d882b2a237d56a6042
Dmitrii-89 <kabasya89@mail.ru>
2024-09-09 22:59:29 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,17 +1,12 @@
 my_string = input('Введите Ваше имя и фамилию: ')
 print(len(my_string))
 
-my_string = input('Введите Ваше имя и фамилию: ')
 print(my_string.upper())
 
-my_string = input('Введите Ваше имя и фамилию: ')
 print(my_string.upper().lower())
 
-my_string = input('Введите Ваше имя и фамилию: ')
 print(my_string.replace(' ',''))
 
-my_string = input('Введите Ваше имя и фамилию: ')
 print(my_string[0])
 
-my_string = input('Введите Ваше имя и фамилию: ')
 print(my_string[-1])
